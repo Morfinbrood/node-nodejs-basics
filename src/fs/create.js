75504d8fc82b20aeeb088c594c314b8975fbe347
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-23 18:15:28 +0300

    Add create.js logic

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import { writeFile } from 'node:fs/promises';
 const create = async () => {
-    // Write your code here 
+    const path = 'src/fs/files/fresh.txt';
+    const data = 'I am fresh and young';
+    try {
+        await writeFile(path, data, { flag: 'ax+' });
+    } catch (err) {
+        throw new Error ("FS operation failed");
+    }
 };
 
 await create();
