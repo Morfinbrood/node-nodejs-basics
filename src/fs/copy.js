1ea8152a583cf39ab476ffdc1a78c3829c56ecf9
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-23 18:45:26 +0300

    Add copy files logic

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
 const copy = async () => {
-    // Write your code here 
+    const path = 'src/fs/files/';
+    const destPath = 'src/fs/files_copy/';
+
+    try {
+        const files = await readdir(path);
+        await mkdir(destPath, { recursive: false });
+        for (const file of files) {
+            await copyFile(path + file, destPath + file);
+        }
+    } catch (err) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await copy();
