2ff7ef42f06fda52a59c7f4a941001cfda81cd40
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-24 16:46:49 +0300

    Feat: Add delete logic

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import { rm } from 'node:fs/promises';
 const remove = async () => {
-    // Write your code here 
+    const path = 'src/fs/files/fileToRemove.txt';
+    try {
+        await rm(path);
+    } catch (err) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await remove();
