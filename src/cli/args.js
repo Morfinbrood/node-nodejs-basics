757c9bc53a73a69f5b7e07f28591e37a9f2bd548
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-24 17:44:54 +0300

    Feat: Add args logic

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const argMap = new Map();
+    for (let i = 2; i < argv.length; i = i + 2) {
+        argMap.set(argv[i], argv[i + 1])
+    }
+    console.log(argMap);
+    // I think this output is more preferable that just a string
+    // if this is problem - pls write me in discord
 };
 
 parseArgs();
