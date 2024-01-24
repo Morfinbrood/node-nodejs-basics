57405e3a2b6a767176de58dbb48f6ded1e4906b0
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-24 17:01:19 +0300

    Feat: Add read logic

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { readFile } from 'node:fs/promises';
 const read = async () => {
-    // Write your code here 
+    const path = 'src/fs/files/fileToRead.txt';
+
+    try {
+        const res = await readFile(path, { encoding: 'utf8' });
+        console.log(res);
+    } catch (err) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await read();
