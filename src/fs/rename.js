f642c6bd3817ac692e3fc9427af45c07b70f1ee0
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-24 16:43:19 +0300

    feat: Add rename logic

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { rename as renameFS, access, constants } from 'node:fs/promises';
 const rename = async () => {
-    // Write your code here 
+    const oldPath = 'src/fs/files/wrongFilename.txt';
+    const newPath = 'src/fs/files/properFilename.md';
+    try {
+        const isNewPathStillExisted = await access(newPath, constants.F_OK);
+        if (isNewPathStillExisted) {
+            throw new Error("FS operation failed");
+        }
+        await renameFS(oldPath, newPath);
+    } catch (err) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await rename();
