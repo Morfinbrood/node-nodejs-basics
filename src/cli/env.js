2b78d92d9d1cb82a9b899d7a4d1cad6abf0c92b5
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-24 17:20:29 +0300

    Feat: Add env logic

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (let property in process.env) {
+        if (property.substring(0, 4) === "RSS_") {
+            console.log(`${property}=${process.env[property]}`)
+        }
+    }
 };
 
 parseEnv();
