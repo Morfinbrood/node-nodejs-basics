ca7f3e42e899139543d4234b00e42cbff3d7fe4b
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-27 20:26:27 +0300

    feat: add hash logic

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const path = './src/hash/files/fileToCalculateHashFor.txt';
+    const hash = crypto.createHash('sha256');
+
+    try {
+        const fileStream = createReadStream(path);
+
+        fileStream.on('data', (data) => {
+            hash.update(data);
+        });
+
+        fileStream.on('end', () => {
+            const calculatedHash = hash.digest('hex');
+            console.log('SHA256 Hash:', calculatedHash);
+        });
+
+        fileStream.on('error', (error) => {
+            console.error('Error reading file:', error.message);
+        });
+    } catch (error) {
+        console.error('Error opening file:', error.message);
+    }
 };
 
 await calculateHash();
