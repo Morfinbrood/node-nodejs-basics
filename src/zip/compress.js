f7504cbb3d520a7e6465aa335709f2e84c207418
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-29 15:01:33 +0300

    Feat: add compress and decompress logics

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const inputFilePath = './src/zip/files/fileToCompress.txt';
+    const outputFilePath = './src/zip/files/archive.gz';
+
+    const readStream = fs.createReadStream(inputFilePath, { encoding: 'utf-8' });
+    const writeStream = fs.createWriteStream(outputFilePath);
+    const gzipStream = zlib.createGzip();
+    readStream.pipe(gzipStream).pipe(writeStream);
+    readStream.on('error', (err) => {
+        console.error(`Error reading the file: ${err.message}`);
+    });
+
+    writeStream.on('error', (err) => {
+        console.error(`Error writing to the compressed file: ${err.message}`);
+    });
+
+    gzipStream.on('error', (err) => {
+        console.error(`Error compressing the file: ${err.message}`);
+    });
+
+    writeStream.on('finish', () => {
+        console.log(`File compressed successfully. Compressed file: ${outputFilePath}`);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFilePath = './src/zip/files/archive.gz';
+    const outputFilePath = './src/zip/files/fileToCompress.txt';
+
+    const readStream = fs.createReadStream(inputFilePath);
+    const writeStream = fs.createWriteStream(outputFilePath, { encoding: 'utf-8' });
+    const gunzipStream = zlib.createGunzip();
+    readStream.pipe(gunzipStream).pipe(writeStream);
+    readStream.on('error', (err) => {
+        console.error(`Error reading the compressed file: ${err.message}`);
+    });
+
+    writeStream.on('error', (err) => {
+        console.error(`Error writing to the decompressed file: ${err.message}`);
+    });
+
+    gunzipStream.on('error', (err) => {
+        console.error(`Error decompressing the file: ${err.message}`);
+    });
+
+    writeStream.on('finish', () => {
+        console.log(`File decompressed successfully. Decompressed file: ${outputFilePath}`);
+    });
 };
 
 await decompress();
