78c2f61273a1a53c914d12c071987996585a8d52
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-01-29 18:44:00 +0300

    Feat: Add ChildProcess logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', ['./src/cp/files/script.js', ...args], {
+        stdio: ['pipe', 'pipe', 'inherit'],
+        encoding: 'utf-8',
+    });
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    });
+
+    childProcess.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+        process.exit(code);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
